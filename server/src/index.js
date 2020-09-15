af0ac8e76ba8b973726063d2377b3fba8b35f8ee
Manassarn Manoonchai <manassarn@gmail.com>
2020-09-15 23:10:52 +0700

    feat(server): add login, booking mutations

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
 const resolvers = require('./resolvers');
@@ -14,7 +15,17 @@ const server = new ApolloServer({
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
-  })
+  }),
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = req.headers && req.headers.authorization || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] || null;
+    return { user: { ...user.dataValues } };
+  },
 });
 
 server.listen().then(({ url }) => {
